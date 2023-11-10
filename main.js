5cac9ae482d3b032b14a88dfc872df66cab052cf
LLYlab <134256418+LLYlab@users.noreply.github.com>
2023-11-10 12:25:51 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 function JPF(){
-windows.alert("We will jump after you are ready!");
-windows.open("https://llylab.github.io/LLYcollection");
+  windows.alert("We will jump after you are ready!");
+  windows.open("https://llylab.github.io/LLYcollection");
+}
+function VisitTF(){
+  var body=document.getElementById("startbody");
+  body.innerHTML="
+  <body id=TFbody>
+    <script src="main.js"></script>
+    <p><strong>大促销，进来即送XKS帅照一张</strong></p>
+    <img src="XKS-TF.png" title="谢老师帅照" alt="Comesome had been shooten down!" width="60px">
+    <a href="XKS-TF.png" download="谢老师帅照">come-some</a>
+    <p><strong>你会下载它！</strong></p>
+    <p></p>
+    <strong>很明显，这个很离谱的网页只是一个测试与恶作剧，所以，请点击右边的按钮！==>></strong>
+    <input type="button" value="LLY colleection启动！" onlick="JPF()">
+  </body>
+</html>
+";
 }
