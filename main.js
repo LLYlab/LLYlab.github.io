9aec526aa4266aaed17589bbf5a3d946f29080f8
LLYlab <134256418+LLYlab@users.noreply.github.com>
2023-11-29 15:57:28 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,56 @@
-function JPF(){
-  windows.alert("We will jump after you are ready!");
-  windows.open("https://llylab.github.io/LLYcollection");
+// main.js
+function showDownloads() {
+    clearContent();
+    document.getElementById('content').innerHTML = `
+        <div id="download-container">
+            <img src="LDCico.png" alt="LDC Icon" id="download-img">
+            <h2>下载 LDC (LLY download center)</h2>
+            <button id="download-btn" onclick="downloadLDC()">下载</button>
+        </div>
+    `;
 }
-function VisitTF(){
-  var body=document.getElementById("startbody");
-  body.innerHTML="<body id=TFbody><script src="https://llylab.github.com/main.js"></script><p><strong>大促销，进来即送XKS帅照一张</strong></p><img src="XKS-TF.png" title="谢老师帅照" alt="Comesome had been shooten down!" width="60px"><a href="XKS-TF.png" download="谢老师帅照">come-some</a><p><strong>你会下载它！</strong></p><p></p><strong>很明显，这个很离谱的网页只是一个测试与恶作剧，所以，请点击右边的按钮！==>></strong><input type="button" value="LLY colleection启动！" onlick="JPF()"></body></html>";
+
+function showStories() {
+    clearContent();
+    // Assuming you have a list of story files in the "story" folder
+    // Replace the file names accordingly
+    const storyFiles = ["story1.txt", "story2.txt", "story3.txt"];
+
+    storyFiles.forEach((fileName) => {
+        const content = fetchStoryContent(fileName);
+        const fileNameWithoutExtension = fileName.replace(".txt", "");
+
+        document.getElementById('content').innerHTML += `
+            <div class="story-container">
+                <h2>${fileNameWithoutExtension}</h2>
+                <p>${content}</p>
+            </div>
+        `;
+    });
 }
-function VisitNovel(){
-  var body=document.getElementById("startbody");
-  body.innerHTML=""
+
+function showData() {
+    clearContent();
+    // Add code to display data content
 }
+
+function showTry() {
+    clearContent();
+    // Add code to display try content
+}
+
+function clearContent() {
+    document.getElementById('content').innerHTML = '';
+}
+
+function downloadLDC() {
+    // Add code to trigger LDC.exe download
+}
+
+function fetchStoryContent(fileName) {
+    // Add code to fetch content of the specified story file
+    // You may need to use AJAX, fetch, or other methods to read the file content
+    // This is a placeholder function, and the implementation depends on your specific requirements.
+    return "Story content goes here.";
+}
+
