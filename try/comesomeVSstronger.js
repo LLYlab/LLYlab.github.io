3d94d5f3784d84db88d0cda46d9247cf4c0a330f
LLYlab <134256418+LLYlab@users.noreply.github.com>
2023-12-08 12:59:58 +0800

    Update comesomeVSstronger.js

--- try/comesomeVSstronger.js
+++ try/comesomeVSstronger.js
@@ -1,19 +1,27 @@
-var cspx=0,cspy=0,qzpx=0,qzpy=0,cswt=0,qzwt=0,csjl=0,qzjl=0,qzjs=0,csh=0,qzh=0;
+var cspx=0,cspy=0,qzpx=0,qzpy=0,cswt=0,qzwt=0,csjl=0,qzjl=0,qzjs=0,csh=0,qzh=0,csmh=0,qzmh=0;
 function reStart(){
-  cspx=;
-  cspy=;
-  qzpx=;
-  qzpy=;
+  cspx=0;
+  cspy=0;
+  qzpx=100;
+  qzpy=0;
 
-  cswt=;
-  csjl=;
-  qzwt=;
-  qzjl=;
-  qzjs=;
+  cswt=0;
+  csjl=0;
+  qzwt=0;
+  qzjl=0;
+  qzjs=0;
 
-  csh=;
-  qzh=;
+  csh=100;//health of cs
+  csmh=100;
+  qzh=1000;//health of stronger
+  qzmh=1000;
 }
 function gameMain(){
-  
+  document.getElementById('game-zone').innerHTML = `
+        <h1>LLYcollection</h1>
+        <div class="menu-option" onclick="showDownloads()">下载</div>
+        <div class="menu-option" onclick="showStories()">故事</div>
+        <div class="menu-option selected" onclick="showData()">资料</div>
+        <div class="menu-option" onclick="showTry()">尝试</div>
+    `;
 }
